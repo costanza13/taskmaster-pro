7ba570964d5d59d379405493337d71fb846342fb
mcc <costanza+git@gmail.com>
2021-04-13 19:47:04 -0700

    audit dates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,37 @@
 var tasks = {};
 
+
+var auditTask = function(taskEl) {
+  // get date from task element
+  var date = $(taskEl).find('span').text().trim();
+  console.log(date);
+
+  // convert to moment object at 5:00pm
+  var time = moment(date, 'L').set('hour', 17);
+  console.log(time);
+
+  // remove any existing due date-related classes from the element
+  $(taskEl).removeClass('list-group-item-warning list-group-item-danger');
+
+  // apply new class id the task is near/over due date
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass('list-group-item-danger');
+  } else if (Math.abs(moment().diff(time, 'days')) <= 2) {
+    $(taskEl).addClass('list-group-item-warning');
+  }
+};
+
 var createTask = function(taskText, taskDate, taskList) {
   // create elements that make up a task item
   var taskLi = $("<li>").addClass("list-group-item");
-  var taskSpan = $("<span>")
-    .addClass("badge badge-primary badge-pill")
-    .text(taskDate);
-  var taskP = $("<p>")
-    .addClass("m-1")
-    .text(taskText);
+  var taskSpan = $("<span>").addClass("badge badge-primary badge-pill").text(taskDate);
+  var taskP = $("<p>").addClass("m-1").text(taskText);
 
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  // check due date
+  auditTask(taskLi);
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
@@ -101,11 +120,20 @@ $('.list-group').on('click', 'span', function() {
   // swap out elements
   $(this).replaceWith(dateInput);
 
+  // enable jquery ui datepicker
+  dateInput.datepicker({
+    minDate: 1,
+    onClose: function() {
+      // when calendar is closed, force a "change" event on the dateInput
+      $(this).trigger('change');
+    }
+  });
+
   // automatically focus on new element
   dateInput.trigger('focus');
 });
 
-$('.list-group').on('blur', 'input[type="text"]', function() {
+$('.list-group').on('change', 'input[type="text"]', function() {
   // get current text
   var date = $(this)
     .val()
@@ -132,6 +160,9 @@ $('.list-group').on('blur', 'input[type="text"]', function() {
   
   // replace textarea with p element
   $(this).replaceWith(taskSpan);
+
+  // Pass task's <li> element into auditTask() to check new due date
+  auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 $('.card .list-group').sortable({
@@ -246,4 +277,6 @@ $("#remove-tasks").on("click", function() {
 // load tasks for the first time
 loadTasks();
 
-
+$('#modalDueDate').datepicker({
+  minDate: 1
+});
